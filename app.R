55c61ce2a191677519aa44aaef009e86dee57df1
Gracie Lovell <146912414+grace-lovell@users.noreply.github.com>
2026-03-22 14:29:53 -0500

    added existing files

---
+++ app.R
@@ -0,0 +1,48 @@
+library(shiny)
+
+apartments <- read_delim("apartments_for_rent_classified_100K.csv", delim = ";")
+crime_40_60 <- read_csv("crime_40_60.csv")
+crime_60_100 <- read_csv("crime_60_100.csv")
+crime_100_250 <- read_csv("crime_100_250.csv")
+crime_250_plus <- read_csv("crime_250_plus.csv")
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white',
+             xlab = 'Waiting time to next eruption (in mins)',
+             main = 'Histogram of waiting times')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
